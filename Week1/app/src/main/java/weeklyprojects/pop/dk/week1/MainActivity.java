12a46b574a5eafdf64febdd314786a88da9ed49e
Rúni Egholm Vørmadal <runivormadal@outlook.com>
2016-09-03 01:27:35 +0200

    added extra feature including using timer updating ui

--- Week1/app/src/main/java/weeklyprojects/pop/dk/week1/MainActivity.java
+++ Week1/app/src/main/java/weeklyprojects/pop/dk/week1/MainActivity.java
@@ -1,9 +1,13 @@
 package weeklyprojects.pop.dk.week1;
 
+import android.content.Context;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.URLUtil;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -12,12 +16,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView txtStatus;
     private Button btnSettings, btnGuide, btnGo;
     private EditText inputAddress;
     private WebView webView;
+    private String defaultStatus = "try ignore http prefix";
+
+    private Timer timer;
+    private TimerTask task;
+    final Handler handler = new Handler();
+    final Runnable runnable = new Runnable() {
+        public void run() {
+            txtStatus.setText(defaultStatus);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +42,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         this.txtStatus = (TextView) findViewById(R.id.txtStatus);
+        this.txtStatus.setText(defaultStatus);
+        this.timer = new Timer();
+
+
         this.btnGuide = (Button) findViewById(R.id.btnGuide);
         this.btnSettings = (Button) findViewById(R.id.btnSettings);
         this.btnGo = (Button) findViewById(R.id.btnGo);
         this.webView = (WebView) findViewById(R.id.webView);
+        this.webView.getSettings().setJavaScriptEnabled(true);
         this.inputAddress = (EditText) findViewById(R.id.inputAddress);
 
         this.txtStatus.setOnClickListener(this);
@@ -51,27 +73,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+    public void setStatusText(String text){
+        txtStatus.setText(text);
+        handler.postDelayed(runnable,2000);
+
+/*        if(task != null) task.cancel();
+
+        task = new TimerTask() {
+            @Override
+            public void run() {
+               handler.handleMessage(null);
+            }
+        };
+        timer.schedule(task, 2000);*/
+    }
+
+
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.btnGuide:
-                txtStatus.setText("You pressed guide button");
+                setStatusText("You pressed guide button");
                 break;
             case R.id.btnSettings:
-                txtStatus.setText("You pressed settings button");
+                setStatusText("You pressed settings button");
                 break;
             case R.id.btnGo:
                     String address = inputAddress.getText().toString();
+                    String statusMsg = "URL is valid";
+                    if(!address.startsWith("http://") && !address.startsWith("https://")){
+                        address = "http://lmgtfy.com/?q=" + address.replace(" ", "+");
+
+                        statusMsg = "let me google that for you";
+                    }
 
                     if(URLUtil.isValidUrl(address)) {
                         webView.loadUrl(address);
-                        txtStatus.setText("URL is valid");
+                        setStatusText(statusMsg);
                     }else {
-                        txtStatus.setText("your url is invalid. remember to add http:// as prefix");
+                        setStatusText("your url is invalid. remember to add http:// as prefix");
                     }
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(inputAddress.getWindowToken(), 0);
                 break;
             default:
-                txtStatus.setText("I Have no idea what you did");
+                setStatusText("I Have no idea what you did");
                 break;
         }
     }
